52ab87fece0fd60bf002dd3c4feda2ee57aa1d76
“aku98” <akashghanate98@gmail.com>
2019-11-30 02:38:52 +0530

    forming multiple images using cv2.pyrUp/cv2.pyrDown fun

---
+++ imageManipulation/imagePyramid.py
@@ -0,0 +1,18 @@
+#Image pyramids Scaling are useful is Object Detection
+import cv2
+import numpy as np 
+
+input=cv2.imread('/home/akashkg/test_imgs/img8.png')
+
+#scaling it down to 50%
+smaller=cv2.pyrDown(input)
+
+#scaling it up to 50%
+larger=cv2.pyrUp(input)
+
+cv2.imshow("Small",smaller)
+cv2.imshow("Large",larger)
+cv2.waitKey()
+cv2.destroyAllWindows()
+
+#multiple images can be formed by looping using small/large image as input to further iterationsg

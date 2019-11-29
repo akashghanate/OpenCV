9674e2015b0ca27d7825ef6ed42336d9f6484cdb
“aku98” <akashghanate98@gmail.com>
2019-11-30 03:56:05 +0530

    brighten or darken image using cv2.add()/cv2.subtract()

---
+++ imageManipulation/arithmeticOper.py
@@ -0,0 +1,22 @@
+#Operations as add and subtract are use to vary the
+#intensties of color in the image ( brightness )
+import cv2
+import numpy as np 
+
+input=cv2.imread('/home/akashkg/test_imgs/img8.png')
+ 
+#create an matrix of ones with dimensions of original image
+# multiple an scaler value of desired to intensify
+M=np.ones(input.shape,dtype="uint8") * 75
+
+#to see increase in brightness add the matrix using cv2.add()
+added=cv2.add(input,M)
+cv2.imshow("added",added)
+cv2.waitKey()
+cv2.destroyAllWindows()
+
+#to see decrease in brightness subtract the matrix using cv2.subtract()
+subtracted=cv2.subtract(input,M)
+cv2.imshow("subtract",subtracted)
+cv2.waitKey()
+cv2.destroyAllWindows()

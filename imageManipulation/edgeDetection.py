30d4a914b9aa70838ecff6b14d4377791e44b395
“aku98” <akashghanate98@gmail.com>
2019-11-30 16:44:24 +0530

    canny is best edge detection algorithm

---
+++ imageManipulation/edgeDetection.py
@@ -0,0 +1,32 @@
+#Edge detection and image gradient using sobel,laplacian and canny
+import cv2
+import numpy as np
+ 
+image=cv2.imread('/home/akashkg/test_imgs/img10.jpg')
+cv2.imshow("Original",image)
+cv2.waitKey()
+
+height, width=image.shape[:2]
+
+#extract sobel edges
+sobel_x=cv2.Sobel(image,cv2.CV_64F,0,1,ksize=5)
+sobel_y=cv2.Sobel(image,cv2.CV_64F,1,0,ksize=5)
+
+cv2.imshow("sobel X",sobel_x)
+cv2.imshow("sobel Y",sobel_y)
+cv2.waitKey()
+
+sobel_or=cv2.bitwise_or(sobel_x,sobel_y)
+cv2.imshow("OR",sobel_or)
+cv2.waitKey()
+
+laplacian=cv2.Laplacian(image,cv2.CV_64F)
+cv2.imshow("laplacian",laplacian)
+cv2.waitKey()
+
+#cv2.canny(image, threshold1, threshold 2) adjusting between the threshold gives proper edges
+canny=cv2.Canny(image,20,170)
+cv2.imshow("canny",canny)
+cv2.waitKey()
+
+cv2.destroyAllWindows()

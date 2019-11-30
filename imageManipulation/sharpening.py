431909a45cd387024bc14924a90d63098b746ebb
“aku98” <akashghanate98@gmail.com>
2019-11-30 05:41:31 +0530

    sharpening the image by normalizing kernel

---
+++ imageManipulation/sharpening.py
@@ -0,0 +1,19 @@
+#it's opposite to blurring, it strengthens the edges
+import cv2
+import numpy as np 
+
+input=cv2.imread('/home/akashkg/test_imgs/img8.png')
+
+# kernel= |-1 -1 -1 |
+#         |-1  9 -1 |
+#         |-1 -1 -1 |
+# it sums up to 1
+
+kernel=np.array([[-1,-1,-1],
+                 [-1, 9,-1],
+                 [-1,-1,-1]])
+
+sharpened=cv2.filter2D(input,-1,kernel)
+cv2.imshow("sharpening",sharpened)
+cv2.waitKey()
+cv2.destroyAllWindows()
